a5e542a6b50e6705549cf3f65ceec89f45715834
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-11-13 07:19:33 -0500

    feat: add some buttons for quick navigation
    add some buttons for quick navigation

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,62 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useSubEnv } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
+import { Domain } from "@web/core/domain";
 
-class AwesomeDashboard extends Component {}
+import { useService } from "@web/core/utils/hooks";
 
-AwesomeDashboard.components = {};
+class AwesomeDashboard extends Component {
+  setup() {
+    useSubEnv({
+      config: {
+        ...getDefaultConfig(),
+        ...this.env.config,
+      },
+    });
+
+    this.display = {
+      controlPanel: { "top-right": false, "bottom-right": false },
+    };
+
+    this.action = useService("action");
+  }
+
+  openCustomerView() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  openOrders(title, domain) {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: title,
+      res_model: "awesome_tshirt.order",
+      views: [[false, "form"]],
+      search_view_id: [false],
+      domain: new Domain(domain).toList(),
+      views: [
+        [false, "list"],
+        [false, "form"]
+      ]
+    });
+  }
+
+  openLast7DaysOrders() {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+    this.openOrders("Last 7 days orders", domain);
+  }
+
+  openLast7DaysCancelledOrders() {
+    const domain =
+      "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state', '=', 'cancelled')]";
+    this.openOrders("Last 7 days cancelled orders", domain);
+  }
+}
+
+AwesomeDashboard.components = { Layout };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
