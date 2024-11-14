f3a71a74aa90bc98ed724a0991b3c726e016be4c
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-11-14 10:29:08 -0500

    feat: implement a field component for image preview
    implement a field component for image preview

---
+++ awesome_tshirt/static/src/image_preview_field/image_preview_field.js
@@ -0,0 +1,13 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl"
+import { registry } from "@web/core/registry";
+import { CharField } from "@web/views/fields/char/char_field";
+
+class ImagePreviewField extends Component {}
+
+ImagePreviewField.template = "awesome_tshirt.ImagePreviewField";
+ImagePreviewField.components = { CharField };
+ImagePreviewField.supportedTypes = ["char"];
+
+registry.category("fields").add("image_preview", ImagePreviewField);
