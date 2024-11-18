ecfb02fa5548129aaea4174e9a86c366ba6c93d5
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-11-18 09:56:14 -0500

    feat: implement command to open image in new tab
    implement command to open image in new tab

--- awesome_tshirt/static/src/image_preview_field/image_preview_field.js
+++ awesome_tshirt/static/src/image_preview_field/image_preview_field.js
@@ -1,10 +1,17 @@
 /** @odoo-module */
 
-import { Component } from "@odoo/owl"
+import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { CharField } from "@web/views/fields/char/char_field";
 
-class ImagePreviewField extends Component {}
+import { useCommand } from "@web/core/commands/command_hook";
+class ImagePreviewField extends Component {
+  setup() {
+    useCommand("Open image in a new tab", () => {
+      window.open(this.props.value, "_blank");
+    });
+  }
+}
 
 ImagePreviewField.template = "awesome_tshirt.ImagePreviewField";
 ImagePreviewField.components = { CharField };
