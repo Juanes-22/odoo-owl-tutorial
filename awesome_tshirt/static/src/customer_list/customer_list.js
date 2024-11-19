f82fc64a22ced5b5c333807ca365d26e3867df31
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-11-19 17:06:09 -0500

    feat: implement customer list custom kanban view
    implement customer list custom kanban view

---
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -0,0 +1,22 @@
+/** @odoo-module **/
+
+import { Component, onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+
+export class CustomerList extends Component {
+  setup() {
+    this.orm = useService("orm");
+    onWillStart(async () => {
+      this.partners = await this.orm.searchRead(
+        "res.partner",
+        [],
+        ["display_name"]
+      );
+    });
+  }
+}
+
+CustomerList.template = "awesome_tshirt.CustomerList";
+CustomerList.props = {
+  selectCustomer: { type: Function },
+};
